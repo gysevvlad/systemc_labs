abdaf80a782b165570049703ccd154ccccd7c091
gysevvlad <gysevvlad@gmail.com>
2016-11-12 09:16:41 +0300

    Added SC_MODULE(bus_t) and testbach

---
+++ bus.h
@@ -0,0 +1,59 @@
+#pragma once
+
+#include <systemc.h>
+
+template <unsigned int device_number>
+SC_MODULE(bus_t)         
+{
+    sc_in< bool >         clk_in;
+    sc_out< bool >        clk_out;
+
+    sc_in< bool >         hwrite_in;
+    sc_out< bool >        hwrite_out;    
+
+    sc_in< sc_uint<32> >  haddr_in;
+    sc_out< sc_uint<32> > haddr_out;
+
+    sc_in< sc_uint<32> >  hwdata_in;
+    sc_out< sc_uint<32> > hwdata_out;
+
+    sc_out< sc_uint<32> > hrdata_out;
+    sc_in< sc_uint<32> >  hrdata_s[device_number];
+
+    sc_out< bool >        hsel_s[device_number];
+
+    void clk_assign()    { clk_out.write(clk_in.read()); }
+    void hwrite_assign() { hwrite_out.write(hwrite_in.read()); }
+    void haddr_assign()  { haddr_out.write(haddr_in.read()); }
+    void hwdata_assign() { hwdata_out.write(hwdata_in.read()); }
+
+    int selected_device;
+    
+    void select_device() {
+        // [0x00000000, 0x0000FFFF]     
+        sc_uint<32> addr(haddr_in.read());
+        selected_device = (addr <= 0x0000FFFF ? addr >> 12 : -1);
+
+        std::cout << "selected device: " << selected_device << std::endl;
+
+        for ( unsigned int i = 0; i < device_number; ++i)
+            hsel_s[i].write(i != selected_device);
+
+        if (selected_device < device_number && selected_device != -1)
+            hrdata_out.write(hrdata_s[selected_device].read());
+        else 
+            hrdata_out.write(sc_uint<32>(0));
+    }
+
+    SC_CTOR(bus_t) : selected_device(-1) {
+        SC_METHOD(clk_assign);    sensitive << clk_in;
+        SC_METHOD(hwrite_assign); sensitive << hwrite_in;
+        SC_METHOD(haddr_assign);  sensitive << haddr_in;
+        SC_METHOD(hwdata_assign); sensitive << hwdata_in;
+
+        SC_METHOD(select_device); 
+        sensitive << haddr_in;
+        for (unsigned int i = 0; i < device_number; ++i)
+            sensitive << hrdata_s[i];
+    }
+};
---
+++ bus_testbanch.cpp
@@ -0,0 +1,64 @@
+#include "bus.h"
+
+int sc_main(int argc, char * argv[])
+{
+    sc_clock clk_in("clk", 10, SC_NS);
+    bus_t<2> bus("bus"); 
+
+    sc_signal< bool > clk_out,
+                      hwrite_in, hwrite_out,
+                      hsel_s1,   hsel_s2;
+    sc_signal< sc_uint<32> > haddr_in,  haddr_out,
+                             hwdata_in, hwdata_out,
+                             hrdata_s1, hrdata_s2,
+                             hrdata_out;
+
+    bus(clk_in,    clk_out, 
+        hwrite_in, hwrite_out,
+        haddr_in,  haddr_out,
+        hwdata_in, hwdata_out,
+        hrdata_out, 
+        hrdata_s1, hrdata_s2,
+        hsel_s1,   hsel_s2);
+
+    sc_trace_file * file_trace = sc_create_vcd_trace_file("bus_testbanch");
+    file_trace->set_time_unit(1.0, SC_PS);
+
+#define s(name) sc_trace(file_trace, name, #name)
+    s(clk_in);     s(hwdata_in);
+    s(clk_out);    s(hwdata_out);
+    s(hwrite_out); s(hrdata_out);
+    s(hwrite_in);  s(hrdata_s1);
+    s(haddr_in);   s(hrdata_s2);
+    s(haddr_out);  s(hsel_s1);
+    s(hsel_s2);
+#undef s
+
+    haddr_in = 0xFFFFFFFF;
+    hrdata_s1 = 0x11111111;
+    hrdata_s2 = 0x22222222;
+
+    sc_start(10, SC_NS);
+
+    hwrite_in = true;
+    haddr_in = 0xA5A5A5A5;
+    hwdata_in = 0x5A5A5A5A;
+    sc_start(10, SC_NS);
+
+    hwrite_in = false;
+    haddr_in = 0x00000000;
+    sc_start(10, SC_NS);
+
+    hrdata_s1 = 0x01010101;
+    sc_start(10, SC_NS);
+
+    haddr_in = 0x00010000;
+    sc_start(10, SC_NS);
+
+    haddr_in = 0x00001000;
+    sc_start(10, SC_NS);
+
+    sc_start(5, SC_NS);
+    sc_close_vcd_trace_file(file_trace);
+    return 0;
+}
